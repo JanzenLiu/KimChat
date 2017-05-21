1ed50f54cb688264f0e85ff7a31337a02ee27c25
= <chengtsung_liu@yahoo.com>
2017-05-21 18:16:41 +0800

    add instructions for stop and restart auto-replying

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import itchat
 from itchat.content import TEXT, PICTURE
 from MsgHandler import *
-from config import signature, header
 
 @itchat.msg_register(TEXT)
 def simply_reply(msg):
 	msg.text = handleInMsg(msg.text)
 	print("In: %s" % msg.text)
 	reply = getReply(msg)
-	reply = handleOutMsg(reply)
-	print("Out: %s" % reply)
-	return(header + reply)
+	if(reply):
+		reply = handleOutMsg(reply)
+		print("Out: %s" % reply)
+		return(header + reply)
 @itchat.msg_register(PICTURE)
 def sticker_reply(msg):
 	print(msg)
